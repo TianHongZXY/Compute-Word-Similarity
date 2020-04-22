e4c2d57d3b7e8a78d71d441bbce5f91bb9b03024
xinyuzhu <zhuxinyu886@163.com>
2020-04-22 12:11:53 +0800

    device bugs

--- calc_sim.py
+++ calc_sim.py
@@ -20,7 +20,7 @@ class similarity_model(nn.Module):
         wordid = wordid.to(self.args.device)
         wordvec = self.embedding(wordid)
         sim_score = torch.matmul(wordvec, self.embedding.weight.data.T)
-        sim_score = sim_score.detach().numpy().tolist()[0]
+        sim_score = sim_score.cpu().numpy().tolist()[0]
         wordids = list(range(self.vocab_size))
         zip_of_score_id = zip(sim_score, wordids)
         zip_of_score_id = sorted(zip_of_score_id,reverse=True, key=lambda x: x[0]) # 根据sim_score对wordids排序
